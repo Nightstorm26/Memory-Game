7cb4498e9bc286a9e4213035fb125ec551ca567f
Nightstorm26 <100034042+Nightstorm26@users.noreply.github.com>
2026-08-31 16:33:09 +0530

    Matching Logic Added

--- scripts.js
+++ scripts.js
@@ -1,7 +1,43 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFLippedCard = false;
+let firstCard, secondCard;
+
+//Card Flip function & Matching logic
 function flipCard(){
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+
+function checkForMatch(){
+    if(firstCard.dataset.framework === secondCard.dataset.framework){
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCArd);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    },1500);
 }
 
 cards.forEach(card => card.addEventListener("click", flipCard));
